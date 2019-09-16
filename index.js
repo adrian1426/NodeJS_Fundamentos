166a14c49b61fa7619ee303138d1e56d49586907
adrian hernandez <adrianhh1426@gmail.com>
2019-09-15 19:58:24 -0500

    creación e importación de modules

---
+++ index.js
@@ -0,0 +1,4 @@
+const operaciones = require('./matematicas');
+console.log(operaciones);
+
+console.log(operaciones.add(1,2));
---
+++ matematicas.js
@@ -0,0 +1,22 @@
+function add(x1,x2){
+    return x1 + x2;
+}
+
+function substract(x1,x2){
+    return x1 - x2;
+}
+
+function multiply(x1,x2){
+    return x1 * x2;
+}
+
+function divide(x1,x2){
+   if (x2 === 0){
+       console.log('no se peuede dividir x 0');
+   }
+   else{
+    return x1 / x2;
+   }
+}
+
+exports.add = add;//modulos , creando modulos
