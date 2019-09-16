b02f58c3e465e4c374cbb290d325eae220c62a7c
adrian hernandez <adrianhh1426@gmail.com>
2019-09-16 15:16:18 -0500

    node y express

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 //creando servidor web
 
-const http = require('http');
+// const http = require('http');
 
 const colors = require('colors');
 
-const handleServer = function(req,res){
-    res.writeHead(200,{'Content-type': 'text/html'});
-    res.write('<h1>Hola mundo desde nodejs</h1>');
-    res.end();
-};
+// const handleServer = function(req,res){
+//     res.writeHead(200,{'Content-type': 'text/html'});
+//     res.write('<h1>Hola mundo desde nodejs</h1>');
+//     res.end();
+// };
 
-const server = http.createServer(handleServer);
+// const server = http.createServer(handleServer);
 
-server.listen(3000,function(){
-    console.log('servidor en puerto 3000'.yellow);
+// server.listen(3000,function(){
+//     console.log('servidor en puerto 3000'.yellow);
+// });
+
+const express = require('express');
+const app= express();
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Hola mundo con express</h1>');
+});
+
+app.listen(3000, ()=>{
+    console.log('escuchando en puerto 3000'.red);
 });
