e5f9594bb2e9f54a4959e297d95fd1ba17bbb88e
adrian hernandez <adrianhh1426@gmail.com>
2019-09-16 15:04:44 -0500

    creando servidores y agregando modulos en nodejs

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const os = require('os');
-const fs = require('fs');
+//creando servidor web
 
-console.log(os.platform());
-console.log(os.release());
-console.log(os.freemem());
-console.log(os.totalmem());
+const http = require('http');
 
-fs.writeFile('./texto.txt','contenido txt',function(error){
-    if(error){
-        console.log(error);
-    }
-    else
-    {
-        console.log('archivo creado');
-    }
+const colors = require('colors');
+
+const handleServer = function(req,res){
+    res.writeHead(200,{'Content-type': 'text/html'});
+    res.write('<h1>Hola mundo desde nodejs</h1>');
+    res.end();
+};
+
+const server = http.createServer(handleServer);
+
+server.listen(3000,function(){
+    console.log('servidor en puerto 3000'.yellow);
 });
-
-console.log('última linea de codigo');
